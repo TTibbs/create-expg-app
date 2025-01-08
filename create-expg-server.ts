49fb161b570a48a60ce819cce6aa9ec7982a83e4
TTibbs <terryjw.97@gmail.com>
2025-01-08 00:48:57 +0000

    Feat: Added the option between JS and TS

--- create-expg-server.ts
+++ create-expg-server.ts
@@ -64,9 +64,20 @@ node_modules/
   const hasGitHubRepo: string = (
     await askQuestion("Do you have a GitHub repository? (y/n) ")
   ).toLowerCase();
+  if (hasGitHubRepo !== "y" && hasGitHubRepo !== "n") {
+    console.error("Invalid input. Please enter 'y' or 'n'.");
+    process.exit(1);
+  }
   let repoUrl: string = "";
   if (hasGitHubRepo === "y") {
     repoUrl = await askQuestion("GitHub repository URL: ");
+  }
+  const isTypeScript: string = await askQuestion(
+    "Do you want to use TypeScript? (y/n) "
+  );
+  if (isTypeScript !== "y" && isTypeScript !== "n") {
+    console.error("Invalid input. Please enter 'y' or 'n'.");
+    process.exit(1);
   }
   const targetPath: string = path.resolve(projectName);
 
@@ -75,8 +86,235 @@ node_modules/
   fs.mkdirSync(targetPath, { recursive: true });
 
   // Define the folder and file structure
-  const structure: { [key: string]: string } = {
-    "package.json": `{
+  const structure: { [key: string]: string } =
+    isTypeScript === "y"
+      ? {
+          "package.json": `{
+  "name": "${projectName}",
+  "author": "${authorName}",
+  "version": "1.0.0",
+  "description": "Express app",
+  "main": "listener.ts",
+  "repository": {
+    "type": "git",
+    "url": "git+${repoUrl}.git"
+  },
+  "bugs": {
+    "url": "${repoUrl}/issues"
+  },
+  "homepage": "${repoUrl}#readme",
+  "keywords": [],
+  "license": "MIT",
+  "scripts": {
+    "start": "ts-node listener.ts",
+    "build": "tsc",
+    "seed": "ts-node db/seeds/run-seed.ts"
+  },
+  "dependencies": {
+    "express": "^4.21.1",
+    "pg": "^8.7.3",
+    "pg-format": "^1.0.4",
+    "dotenv": "^16.0.0",
+    "cors": "^2.8.5",
+    "nodemon": "^3.1.7"
+  },
+  "devDependencies": {
+    "@types/express": "^4.17.x",
+    "@types/node": "^20.x.x",
+    "@types/cors": "^2.8.x",
+    "@types/pg": "^8.x.x",
+    "@types/jest": "^27.x.x",
+    "@types/supertest": "^2.x.x",
+    "ts-node": "^10.x.x",
+    "typescript": "^5.x.x",
+    "ts-jest": "^29.x.x"
+  },
+  "jest": {
+    "setupFilesAfterEnv": [
+      "jest-extended/all",
+      "jest-sorted"
+    ]
+  }
+}`,
+          "tsconfig.json": `{
+  "compilerOptions": {
+    "target": "ES6",
+    "module": "commonjs",
+    "outDir": "./dist",
+    "rootDir": "./",
+    "strict": true,
+    "esModuleInterop": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true,
+    "resolveJsonModule": true
+  },
+  "include": ["**/*.ts"],
+  "exclude": ["node_modules", "**/*.test.ts"]
+}`,
+          "__tests__/app.test.ts": `import app from '../app';
+import request from 'supertest';
+import db from '../db/connection';
+import seed from '../db/seeds/seed';
+import data from '../db/data/test-data/index';
+import endpoints from '../endpoints.json';
+require("jest-sorted");
+
+beforeEach(() => seed(data));
+afterAll(() => db.end());
+
+// Build tests here
+`,
+          "db/data/test-data/index.ts": "// Test data",
+          "db/seeds/seed.ts": "// Seed data logic here",
+          "db/seeds/run-seed.ts": `import devData from "../data/development-data/index";
+import seed from "../seeds/seed";
+import db from "../connection";
+
+const runSeed = () => {
+  return seed(devData).then(() => db.end()); 
+};
+
+runSeed();
+`,
+          "db/connection.ts":
+            `const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: path.resolve(__dirname, ` +
+            "`../../.env.${ENV}`" +
+            `),
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable must be set");
+}
+  
+const config = {}
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+  
+export default new Pool(config);
+`,
+          "db/setup.sql": `DROP DATABASE IF EXISTS test_db_name_here;
+DROP DATABASE IF EXISTS db_name_here;
+
+CREATE DATABASE test_db_name_here;
+CREATE DATABASE db_name_here;`,
+          "controllers/users-controller.ts": `import { Request, Response, NextFunction } from 'express';
+import { selectUsers } from "../models/users-models";
+
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await selectUsers();
+    res.status(200).send({ users });
+  } catch (err) {
+    next(err);
+  }
+};
+`,
+          "models/users-models.ts": `import db from "../db/connection";
+
+exports.selectUsers = async () => {
+  return db.query("SELECT * FROM users").then(({ rows }) => {
+    return rows;
+  });
+};
+`,
+          "routes/users-router.ts": `const usersRouter = require("express").Router();
+import { getUsers } from "../controllers/users-controller";
+
+usersRouter.get("/", getUsers);
+
+export default usersRouter;`,
+          "routes/api-router.ts": `import express from "express";
+const apiRouter = express.Router();
+import usersRouter from "./users-router";
+import endpoints from "../endpoints.json";
+
+apiRouter.get("/", (req, res) => {
+  res.status(200).send({ endpoints: endpoints });
+});
+
+apiRouter.use("/users", usersRouter);
+
+module.exports = apiRouter;`,
+          "app.ts": `import express from "express";
+import cors from "cors";
+import apiRouter from "./routes/api-router";
+const app = express();
+import { inputErrorHandler, psqlErrorHandler, customErrorHandler, serverErrorHandler } from "./errors";
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", apiRouter);
+app.use("/api/*", inputErrorHandler);
+app.use(psqlErrorHandler);
+app.use(customErrorHandler);
+app.use(serverErrorHandler);
+
+export default app;
+`,
+          "endpoints.json": `{
+  "GET /api": {
+    "description": "serves up a json representation of all the available endpoints of the api"
+  },
+  "GET /api/users": {
+    "description": "Returns an array of users with the username, name and avatar url",
+    "exampleResponse": [
+      {
+        "username": "10x engineer",
+        "name": "Theo"
+      }
+    ]
+  }
+}`,
+          "listener.ts": `import app from "./app";
+const { PORT = 9090 } = process.env;
+app.listen(PORT, () => console.log("Listening on port", PORT));`,
+          "errors/index.ts": `import { Request, Response, NextFunction } from 'express';
+
+interface CustomError extends Error {
+  status?: number;
+  msg?: string;
+}
+
+exports.inputErrorHandler = (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send({ msg: "Invalid input" });
+  next(err);
+};
+
+exports.psqlErrorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  if (err.code === "23502" || err.code === "22P02" || err.code === "23503") {
+    res.status(400).send({ msg: "Bad request" });
+  } else next(err);
+};
+
+exports.customErrorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+};
+
+exports.serverErrorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  console.log(err, "<<<<<< ------ Unhandled error");
+  res.status(500).send({ msg: "Internal server error" });
+};
+`,
+          ".env.production": "DATABASE_URL=your_production_db_url",
+          ".env.test": "PGDATABASE=your_test_db_name",
+          ".env.development": "PGDATABASE=your_db_name",
+          ".env-example": `PGDATABASE=your_db_name
+PGDATABASE=your_test_db_name
+DATABASE_URL=your_production_db_url
+`,
+        }
+      : {
+          "package.json": `{
   "name": "${projectName}",
   "author": "${authorName}",
   "version": "1.0.0",
@@ -120,7 +358,7 @@ node_modules/
     ]
   }
 }`,
-    "__tests__/app.test.js": `const app = require("../app.js");
+          "__tests__/app.test.js": `const app = require("../app.js");
 const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
@@ -133,9 +371,9 @@ afterAll(() => db.end());
 
 // Build tests here
 `,
-    "db/data/test-data/index.js": "// Test data",
-    "db/seeds/seed.js": "// Seed data logic here",
-    "db/seeds/run-seed.js": `const devData = require("../data/development-data/index.js");
+          "db/data/test-data/index.js": "// Test data",
+          "db/seeds/seed.js": "// Seed data logic here",
+          "db/seeds/run-seed.js": `const devData = require("../data/development-data/index.js");
 const seed = require("../seeds/seed.js");
 const db = require("../connection.js");
 
@@ -145,14 +383,14 @@ const runSeed = () => {
 
 runSeed();
 `,
-    "db/connection.js":
-      `const { Pool } = require("pg");
+          "db/connection.js":
+            `const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
   path: path.resolve(__dirname, ` +
-      "`../../.env.${ENV}`" +
-      `),
+            "`../../.env.${ENV}`" +
+            `),
 });
 
 if (!process.env.DATABASE_URL) {
@@ -168,12 +406,12 @@ if (ENV === "production") {
   
 module.exports = new Pool(config);
 `,
-    "db/setup.sql": `DROP DATABASE IF EXISTS test_db_name_here;
+          "db/setup.sql": `DROP DATABASE IF EXISTS test_db_name_here;
 DROP DATABASE IF EXISTS db_name_here;
 
 CREATE DATABASE test_db_name_here;
 CREATE DATABASE db_name_here;`,
-    "controllers/users-controller.js": `const { selectUsers } = require("../models/users-models.js");
+          "controllers/users-controller.js": `const { selectUsers } = require("../models/users-models.js");
 
 exports.getUsers = (req, res, next) => {
   selectUsers()
@@ -185,7 +423,7 @@ exports.getUsers = (req, res, next) => {
     });
 };
 `,
-    "models/users-models.js": `const db = require("../db/connection.js");
+          "models/users-models.js": `const db = require("../db/connection.js");
 
 exports.selectUsers = async () => {
   return db.query("SELECT * FROM users").then(({ rows }) => {
@@ -193,13 +431,13 @@ exports.selectUsers = async () => {
   });
 };
 `,
-    "routes/users-router.js": `const usersRouter = require("express").Router();
+          "routes/users-router.js": `const usersRouter = require("express").Router();
 const { getUsers } = require("../controllers/users-controller.js");
 
 usersRouter.get("/", getUsers);
 
 module.exports = usersRouter;`,
-    "routes/api-router.js": `const express = require("express");
+          "routes/api-router.js": `const express = require("express");
 const apiRouter = express.Router();
 const usersRouter = require("./users-router.js");
 const endpoints = require("../endpoints.json");
@@ -211,7 +449,7 @@ apiRouter.get("/", (req, res) => {
 apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;`,
-    "app.js": `const express = require("express");
+          "app.js": `const express = require("express");
 const cors = require("cors");
 const apiRouter = require("./routes/api-router.js");
 const app = express();
@@ -233,7 +471,7 @@ app.use(serverErrorHandler);
 
 module.exports = app;
 `,
-    "endpoints.json": `{
+          "endpoints.json": `{
   "GET /api": {
     "description": "serves up a json representation of all the available endpoints of the api"
   },
@@ -247,10 +485,10 @@ module.exports = app;
     ]
   }
 }`,
-    "listener.js": `const app = require("./app.js");
+          "listener.js": `const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 app.listen(PORT, () => console.log("Listening on port", PORT));`,
-    "errors/index.js": `exports.inputErrorHandler = (req, res, next) => {
+          "errors/index.js": `exports.inputErrorHandler = (req, res, next) => {
   res.status(404).send({ msg: "Invalid input" });
   next(err);
 };
@@ -272,14 +510,33 @@ exports.serverErrorHandler = (err, req, res, next) => {
   res.status(500).send({ msg: "Internal server error" });
 };
 `,
-    ".env.production": "DATABASE_URL=your_production_db_url",
-    ".env.test": "PGDATABASE=your_test_db_name",
-    ".env.development": "PGDATABASE=your_db_name",
-    ".env-example": `PGDATABASE=your_db_name
+          ".env.production": "DATABASE_URL=your_production_db_url",
+          ".env.test": "PGDATABASE=your_test_db_name",
+          ".env.development": "PGDATABASE=your_db_name",
+          ".env-example": `PGDATABASE=your_db_name
 PGDATABASE=your_test_db_name
 DATABASE_URL=your_production_db_url
 `,
-  };
+        };
+
+  // Create the files and folders based on the structure
+  for (const [filePath, content] of Object.entries(structure)) {
+    const fullPath = path.join(targetPath, filePath);
+    fs.mkdirSync(path.dirname(fullPath), { recursive: true });
+    fs.writeFileSync(fullPath, content);
+  }
+
+  // Create a .gitignore file
+  const gitignoreContent = `
+node_modules/
+.env.*
+`;
+  createFileOrFolder(
+    path.join(targetPath, ".gitignore"),
+    gitignoreContent.trim()
+  );
+
+  console.log("Project setup complete.");
 
   // Create the folder structure and files
   console.log("Creating project structure...");
@@ -291,8 +548,16 @@ DATABASE_URL=your_production_db_url
   if (hasGitHubRepo === "y") {
     console.log("GitHub repository already exists.");
   } else {
+    console.log("Creating a new GitHub repository...");
     initializeGitRepo(targetPath);
     execSync("git branch -M main", { cwd: targetPath, stdio: "inherit" });
+  }
+
+  if (isTypeScript === "y") {
+    console.log("Setting up TypeScript...");
+    execSync(
+      "npm install --save-dev @types/express @types/express-serve-static-core @types/node"
+    );
   }
 
   // Install dependencies
